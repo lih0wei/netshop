b7b80cb77c8e334096ddce94c1a2090a1516b878
lihongwei <lih0wei@163.com>
2021-03-05 10:46:55 +0800

    wancheng

--- order/urls.py
+++ order/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns=[
     url(r'^$',views.ToOrderView.as_view()),
     url(r'^order.html$',views.OrderListView.as_view()),
+    url(r'^topay/$', views.ToPayView.as_view()),
+    url(r'^checkPay/$', views.CheckPayView.as_view()),
 ]
--- order/views.py
+++ order/views.py
@@ -1,9 +1,14 @@
+from django.db.models import F
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 # Create your views here.
 from django.views import View
 import jsonpickle
-from cart.cartmanager import getCartManger
+from cart.cartmanager import getCartManger, CartItem
+from goods.models import Inventory
+from order.models import Order, OrderItem
+from userapp.models import Address
+from utils.alipay import *
 
 class ToOrderView(View):
     def get(self,request):
@@ -25,20 +30,102 @@ class OrderListView(View):
         print(cartitems)
 
         # 将json格式字符串转换成python对象（字典{goodsid:1,colorid:1,sizeid:1}）列表
-        # [ {goodsid:1,colorid:1,sizeid:1},{goodsid:1,colorid:1,sizeid:1}]
-        cartitemList = jsonpickle.loads("[" + cartitems + "]")
+        #[ {goodsid:1,colorid:1,sizeid:1},{goodsid:1,colorid:1,sizeid:1}]
+        cartitemList = jsonpickle.loads("["+cartitems+"]")
 
         # 将python对象列表转换成CartItem对象列表
         cartitemObjList = [getCartManger(request).get_cartitems(**item) for item in cartitemList if item]
 
-        # 获取用户的默认收货地址
+        #获取用户的默认收货地址
         address = request.session.get('user').address_set.get(isdefault=True)
 
-        # 获取支付总金额
+        #获取支付总金额
         totalPrice = 0
         for cm in cartitemObjList:
+            totalPrice+=cm.getTotalPrice()
+
+
+
+
+        return render(request,'order.html',{'cartitemObjList':cartitemObjList,'address':address,'totalPrice':totalPrice})
+
+
+#创建AliPay对象
+alipay = AliPay(appid='2021000117616622', app_notify_url='http://127.0.0.1:8000/order/checkPay/', app_private_key_path='order/keys/my_private_key.txt',
+                 alipay_public_key_path='order/keys/alipay_public_key.txt', return_url='http://127.0.0.1:8000/order/checkPay/', debug=True)
+
+
+class ToPayView(View):
+    def get(self,request):
+
+        #1.插入Order表中数据
+        #获取请求参数
+        import uuid,datetime
+        data = {
+            'out_trade_num':uuid.uuid4().hex,
+            'order_num':datetime.datetime.today().strftime('%Y%m%d%H%M%S'),
+            'payway':request.GET.get('payway'),
+            'address':Address.objects.get(id=request.GET.get('address','')),
+            'user':request.session.get('user','')
+        }
+
+        print(data)
+
+        orderObj = Order.objects.create(**data)
+
+
+        #2.插入OrderItem表中数据
+        print(request.GET.get('cartitems',''),'==========================')
+
+        cartitems = jsonpickle.loads(request.GET.get('cartitems'))
+
+        print(cartitems)
+
+        orderItemList = [getCartManger(request).get_cartitems(**item) for item in cartitems if item]
+        totalPrice = 0
+        for cm in orderItemList:
             totalPrice += cm.getTotalPrice()
 
-        return render(request, 'order.html',
-                      {'cartitemObjList': cartitemObjList, 'address': address, 'totalPrice': totalPrice})
+        #3.获取扫码支付的页面
+        params = alipay.direct_pay(subject='京东超市', out_trade_no=orderObj.out_trade_num, total_amount=str(totalPrice))
+
+        #拼接请求地址
+        url = alipay.gateway+'?'+params
+
+        return HttpResponseRedirect(url)
+
+
+class CheckPayView(View):
+    def get(self,request):
+        #校验是否支付成功（验签的过程）
+        params = request.GET.dict()
+        #获取签名
+        sign = params.pop('sign')
+
+        if alipay.verify(params,sign):
+            #修改订单表中的支付状态
+            out_trade_no = params.get('out_trade_no','')
+            order = Order.objects.get(out_trade_num=out_trade_no)
+            order.status = u'待发货'
+            order.save()
+
+            #修改库存
+            orderitemList = order.orderitem_set.all()
+            [Inventory.objects.filter(goods_id=item.goodsid,size_id=item.sizeid,color_id=item.colorid).update(count=F('count')-item.count) for item in orderitemList if item]
+
+            # 修改购物表
+            [CartItem.objects.filter(goodsid=item.goodsid,sizeid=item.sizeid,colorid=item.colorid).delete() for item in orderitemList if item]
+
+
+            return HttpResponse('支付成功！')
+
+        return HttpResponse('支付失败！')
+
+
+
+
+
+
+
+
 
